ef67f25c1ce44e911dce61b1f3ab0cecd9224843
Ubuntu <ubuntu@ip-172-31-29-125.us-west-2.compute.internal>
2013-07-23 16:47:23 +0000

    add bitstarter

---
+++ coursera/bitstarter/web.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+var fs = require('fs'),
+
+buf = fs.readFileSync('index.html').toString('utf-8');
+app.get('/', function(request, response) {
+  response.send(buf);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
